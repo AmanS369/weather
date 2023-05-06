8aed26ab64c5ba098815d2eaf4815d88c3d54df6
AmanS369 <91814909+AmanS369@users.noreply.github.com>
2023-05-06 13:01:21 +0530

    Add files via upload

---
+++ weather/frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import Weather_Card from './component/Weather_Card'
+
+function App() {
+  return (
+    <div className="App">
+
+      < Weather_Card/>
+    </div>
+  );
+}
+
+export default App;
